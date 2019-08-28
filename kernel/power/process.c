da484c5138c106b8dc6ef3fc30866d4751dd910a
Danny Lin <danny@kdrag0n.dev>
2019-08-27 23:54:00 -0700

    PM / sleep: Skip OOM killer toggles when a LMK is in use
    CONFIG_HAVE_LOW_MEMORY_KILLER disables the OOM killer, so there's no
    reason to disable and enable the OOM killer when entering and exiting
    suspend.
    
    This is a fixed version of https://github.com/YaroST12/VIOLENT_kernel/commit/86e59a93b2ef
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -147,7 +147,7 @@ int freeze_processes(void)
 	pr_cont("\n");
 	BUG_ON(in_atomic());
 
-#ifndef CONFIG_ANDROID
+#ifdef CONFIG_HAVE_LOW_MEMORY_KILLER
 	/*
 	 * Now that the whole userspace is frozen we need to disbale
 	 * the OOM killer to disallow any further interference with
@@ -201,7 +201,7 @@ void thaw_processes(void)
 	pm_freezing = false;
 	pm_nosig_freezing = false;
 
-#ifndef CONFIG_ANDROID
+#ifdef CONFIG_HAVE_LOW_MEMORY_KILLER
 	oom_killer_enable();
 #endif
 
