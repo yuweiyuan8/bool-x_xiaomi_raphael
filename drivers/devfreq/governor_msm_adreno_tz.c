tetsuo55/android_kernel_xiaomi_sm8150

adreno_tz: Make high refresh multiplier configurable through a sysfs node
*This will be useful during testing of different values for highref mulitplier.
*No one wants to compile the whole kernel again and flash it just to change the value of the highref multiplier.

Also add a bp hint.

Signed-off-by: Forenche <prahul2003@gmail.com>

drivers/devfreq/governor_msm_adreno_tz.c
@@ -336,6 +336,9 @@ static inline int devfreq_get_freq_level(struct devfreq *devfreq,
 	return -EINVAL;
 }
 
+int highref_mulitplier_get = CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI;
+module_param(highref_mulitplier_get, uint, 0644);
+
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 {
 	int result = 0;
@@ -392,7 +395,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
 		if (refresh_rate > 60)
-			scm_data[2] = priv->bin.busy_time * CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI / 100;
+			if (likely(CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI != 0))
+				scm_data[2] = priv->bin.busy_time * highref_multiplier_get / 100;
+			else
+				scm_data[2] = priv->bin.busy_time * refresh_rate / 60;
 		else
 			scm_data[2] = priv->bin.busy_time;
 		scm_data[3] = context_count;

yuweiyuan8/bool-x_xiaomi_raphael

Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

drivers/devfreq/governor_msm_adreno_tz.c
@@ -352,6 +352,10 @@ static inline int devfreq_get_freq_level(struct devfreq *devfreq,
 extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq);
 #endif
+
+int highref_multiplier_get = CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI;
+module_param(highref_multiplier_get, uint, 0644);
+
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 {
 	int result = 0;
@@ -422,7 +426,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
 		if (refresh_rate > 60)
-			scm_data[2] = priv->bin.busy_time * CONFIG_DEVFREQ_ADRENO_HIGHREFRESH_MULTI / 100;
+			if (likely(highref_multiplier_get != 0))
+				scm_data[2] = priv->bin.busy_time * highref_multiplier_get / 100;
+			else
+				scm_data[2] = priv->bin.busy_time * refresh_rate / 60;
 		else
 			scm_data[2] = priv->bin.busy_time;
 		scm_data[3] = context_count;